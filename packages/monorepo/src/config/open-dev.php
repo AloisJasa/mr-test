24f47337b49b01625719afdb389809abf52f4cdd
Alois Jaša <aloisjasa@gmail.com>
2023-12-12 14:22:40 +0100

    Nastavení pomocí metod z MBConfig namísto setování přímo do Config přes deprecated konstanty

--- packages/monorepo/src/config/open-dev.php
+++ packages/monorepo/src/config/open-dev.php
@@ -1,12 +1,13 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $containerConfigurator): void {
-	$services = $containerConfigurator->services();
-	$services->set(\AloisJasa\Monorepo\Worker\OpenDev\CheckoutMainWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\OpenDev\SetNextMutualDependenciesReleaseWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\OpenDev\UpdateBranchAliasReleaseWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\OpenDev\UpdateComposerLockWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\OpenDev\PushNextDevReleaseWorker::class);
+	$containerConfigurator->workers([
+		\AloisJasa\Monorepo\Worker\OpenDev\CheckoutMainWorker::class,
+		\AloisJasa\Monorepo\Worker\OpenDev\SetNextMutualDependenciesReleaseWorker::class,
+		\AloisJasa\Monorepo\Worker\OpenDev\UpdateBranchAliasReleaseWorker::class,
+		\AloisJasa\Monorepo\Worker\OpenDev\UpdateComposerLockWorker::class,
+		\AloisJasa\Monorepo\Worker\OpenDev\PushNextDevReleaseWorker::class,
+	]);
 };
--- packages/monorepo/src/config/release-candidate.php
+++ packages/monorepo/src/config/release-candidate.php
@@ -1,16 +1,16 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 return static function (MBConfig $containerConfigurator): void {
-	$services = $containerConfigurator->services();
-
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\CreatePrepareReleaseBranchWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\UpdateReplaceReleaseWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\SetCurrentMutualDependenciesReleaseWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\WriteApplicationVersionWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\SetNextMutualDependenciesReleaseWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\UpdateComposerLockWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\CommitPrepareReleaseWorker::class);
-	$services->set(\AloisJasa\Monorepo\Worker\ReleaseCandidate\PushPrepareReleaseBranchWorker::class);
+	$containerConfigurator->workers([
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\CreatePrepareReleaseBranchWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\UpdateReplaceReleaseWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\SetCurrentMutualDependenciesReleaseWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\WriteApplicationVersionWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\SetNextMutualDependenciesReleaseWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\UpdateComposerLockWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\CommitPrepareReleaseWorker::class,
+		\AloisJasa\Monorepo\Worker\ReleaseCandidate\PushPrepareReleaseBranchWorker::class,
+	]);
 };
