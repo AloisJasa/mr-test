36ff75c1eaa5ed7b0751219196deb2e1ae856be6
Alois Jaša <aloisjasa@gmail.com>
2023-12-13 10:03:39 +0100

    fix

--- packages/monorepo/src/Worker/OpenDev/CheckoutMainWorker.php
+++ packages/monorepo/src/Worker/OpenDev/CheckoutMainWorker.php
@@ -2,19 +2,10 @@
 
 namespace AloisJasa\Monorepo\Worker\OpenDev;
 
-use MonorepoBuilder202211\Symplify\PackageBuilder\Parameter\ParameterProvider;
 use PharIo\Version\Version;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 
 final class CheckoutMainWorker extends AbstractOpenDevWorker
 {
-	public function __construct(
-		private readonly ParameterProvider $parameterProvider,
-	)
-	{
-	}
-
-
 	public function work(Version $version) : void
 	{
 		$gitCheckoutMaster = sprintf('git fetch && git checkout %s', $this->getDefaultBranch());
@@ -24,12 +15,6 @@ final class CheckoutMainWorker extends AbstractOpenDevWorker
 
 	public function getDescription(Version $version) : string
 	{
-		return sprintf('Checkout default branch.', $this->getDefaultBranch());
-	}
-
-
-	private function getDefaultBranch(): string
-	{
-		return $this->parameterProvider->provideStringParameter(Option::DEFAULT_BRANCH_NAME);
+		return sprintf('Checkout default branch "%s".', $this->getDefaultBranch());
 	}
 }
